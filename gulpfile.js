9ec02a2573ec15a86f0a674622c458acb602ab51
GabrielM <gabriel.martinez@banlinea.com>
2018-06-28 18:32:09 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); //Runs a local dev server
+var open = require('gulp-open'); ///Open a URL in a browser
+var browserify = require('browserify'); //Bundles JS
+var reactify = require('reactify'); //Transforms React JSX to JS
+var source = require('vinyl-source-stream'); //use conevntinal text streams with Gulp
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+}
+//Start a local developement server
+gulp.task('connect', function () {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    })
+})
+
+gulp.task('open', ['connect'], function () {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/' }))
+});
+
+gulp.task('html', function () {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle'))
+        .pipe(gulp.dest(config.dist + '/scripts'))
+        .pipe(connect.reload());
+})
+
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, ['html'])
+    gulp.watch(config.paths.js, ['js'])
+})
+gulp.task('default', ['html', 'js', 'open', 'watch']);
